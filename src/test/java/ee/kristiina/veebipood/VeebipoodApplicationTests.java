b7926d06335aedac6c217086c840bfbf79c0c335
KriKind <159944274+KriKind@users.noreply.github.com>
2025-10-29 10:16:32 +0200

    veebipood algus

---
+++ src/test/java/ee/kristiina/veebipood/VeebipoodApplicationTests.java
@@ -0,0 +1,13 @@
+package ee.kristiina.veebipood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VeebipoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
